4ecaa0dc65a8e493e71078c8f3219b5441624caa
Oleg Selivanov <alien3569@mail.ru>
2022-12-18 18:47:24 +0300

    Add fin

--- Task 50/Program.cs
+++ Task 50/Program.cs
@@ -17,34 +17,23 @@ Console.WriteLine("Введите номер строки: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите номер столбца: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int [,] numbers = new int [3,4];
-FillArrayRandomNumbers(numbers);
+int [,] matrix = new int [3,4];
+InputMatrix(matrix);
 
-if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+if (n > matrix.GetLength(0) || m > matrix.GetLength(1))
     Console.WriteLine("Такого элемента нет.");
 else
-    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
-PrintArray(numbers);
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {matrix[n-1,m-1]}");
 
-void FillArrayRandomNumbers(int[,] array)
+void InputMatrix(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
         {        
-            for (int j = 0; j < array.GetLength(1); j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                array [i,j] = new Random().Next(-100, 100)/10;
-            }   
+                matrix[i, j] = new Random().Next(-100, 100) / 10;
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();   
         }
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} \t");
-        }
-        Console.WriteLine(); 
-    }
 }
